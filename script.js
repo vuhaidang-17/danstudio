c5305b75291627dd40c3cd22844d86ac60856568
DANG <votrungtanluc1702@gmail.com>
2026-02-08 16:46:06 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("DAN.STUDIO loaded");
+const form = document.getElementById("contactForm");
+const hint = document.getElementById("hint");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  const name = (data.get("name") || "").toString().trim();
+
+  hint.textContent = `Đã gửi! Cảm ơn ${name || "bạn"} 🙂 (demo thôi, chưa có backend)`;
+  form.reset();
+});
